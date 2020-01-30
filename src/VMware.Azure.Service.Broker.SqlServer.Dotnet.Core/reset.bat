88ff75691c419edc2dfebb2ab0b1112c69c5b39d
nycpivot <mijames@vmware.com>
2020-01-30 15:09:48 -0500

    Added reset.bat.

---
+++ src/VMware.Azure.Service.Broker.SqlServer.Dotnet.Core/reset.bat
@@ -0,0 +1,15 @@
+cf target -s azure-service-broker
+pause
+@cls
+
+cf unbind-service vmware-azure-service-broker-sqlserver-dotnet-core asb-sqldb
+pause
+@cls
+
+cf delete-service asb-sqldb -f
+pause
+@cls
+
+cf vmware-azure-service-broker-sqlserver-dotnet-core -r -f
+pause
+@cls
